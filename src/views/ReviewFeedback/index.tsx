1458d0f4caedc591a8d0ba743c7ef554a69dd6b4
sadewole <samueladewole15@gmail.com>
2022-10-18 17:11:49 +0100

    feat: worked on my-feedback page

--- src/views/ReviewFeedback/index.tsx
+++ src/views/ReviewFeedback/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import Button from '../../components/Button'
 import Feedbacks from '../../components/Feedbacks'
 import MainLayout from '../../layouts/MainLayout'
 import styles from './reviewFeedback.module.css'
@@ -6,7 +6,20 @@ import styles from './reviewFeedback.module.css'
 const ReviewFeedback = () => {
   return (
     <MainLayout loggedIn>
-      <h1>Review Feedback Given</h1>
+      <div className={styles.header}>
+        <h1>My Feedback</h1>
+        <div>
+          <p className={styles.subtitle}>Feedback Period</p>
+          <div className={styles.toptool}>
+            <select name="" id="" className={styles.selectPeriod}>
+              <option value="">All</option>
+              <option value="">October 2018</option>
+              <option value="">November 2018</option>
+            </select>
+            <Button onClick={() => {}}>Publish Feedback</Button>
+          </div>
+        </div>
+      </div>
       <Feedbacks />
     </MainLayout>
   )
