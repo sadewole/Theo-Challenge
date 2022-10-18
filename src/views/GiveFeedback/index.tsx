38687213944c8b8c8ca6a65dd1bc2fa585bfc82e
sadewole <samueladewole15@gmail.com>
2022-10-18 18:46:30 +0100

    feat: filter user and update on share feedbackpage

--- src/views/GiveFeedback/index.tsx
+++ src/views/GiveFeedback/index.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react'
 import { useHistory } from 'react-router-dom'
-import { DispatchQuestionContext } from '../../context/QuestionProvider'
+import {
+  DispatchQuestionContext,
+  QuestionContext,
+} from '../../context/QuestionProvider'
 import { UserContext } from '../../context/UserProvider'
 import MainLayout from '../../layouts/MainLayout'
 import User from '../../components/User'
 import Button from '../../components/Button'
 import styles from './giveFeedback.module.css'
 import { UserT } from '../../context/types'
+import { AccountContext } from '../../context/AccountProvider'
 
 const GiveFeedback = () => {
   const history = useHistory()
+  const currentUser = React.useContext(AccountContext)
   const users = React.useContext(UserContext)
   const questionDispatch = React.useContext(DispatchQuestionContext)
+  const { feedbackList } = React.useContext(QuestionContext)
 
   const handleFillOut = (user: UserT) => {
     questionDispatch({
@@ -22,17 +28,33 @@ const GiveFeedback = () => {
     return history.push('/share-feedback/fill-out')
   }
 
+  const filteredUser =
+    users &&
+    users
+      .filter((user) => user.id !== currentUser?.id)
+      .map((user) => {
+        const isSubmitted = feedbackList.find(
+          (feed) => feed.from.id === currentUser?.id && feed.to.id === user.id,
+        )
+        return { ...user, submitted: !!isSubmitted }
+      })
+
   return (
     <MainLayout loggedIn>
       <div className={styles.wrapper}>
         <h1>Share Feedback</h1>
-        {users && users.length > 0 && (
+        {filteredUser && filteredUser.length > 0 && (
           <ul className={styles.users}>
-            {users.map((user) => (
+            {filteredUser.map((user) => (
               <li key={user.id} className={styles.user}>
                 <User name={user.name} avatarUrl={user.avatarUrl} showName />
                 <span style={{ flex: 1 }} />
-                <Button onClick={() => handleFillOut(user)}>Fill out</Button>
+                <Button
+                  onClick={() => handleFillOut(user)}
+                  secondary={user.submitted}
+                >
+                  {!user.submitted ? 'Fill out' : 'View Submissions'}
+                </Button>
               </li>
             ))}
           </ul>
